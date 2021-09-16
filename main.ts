d92a1c5c10ef61043beb40b83b70a14246fcd695
radamr <rybicki-adam@wp.pl>
2021-09-16 17:15:30 +0200

    nowa animka

--- main.ts
+++ main.ts
@@ -1,3 +1,120 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . . # # .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # . .
+            . # # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            . # # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # #
+            . . . # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # #
+            . . . # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # #
+            . . . # #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . # #
+            . . . # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Rollerskate)
+            basic.showIcon(IconNames.Duck)
+        }
+    }
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.pause(5000)
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
 })
